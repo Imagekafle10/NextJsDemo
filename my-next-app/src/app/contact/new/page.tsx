bfc2da97162c2b86f344c0eadb9c3afe211c8977
Imagekafle10 <hpimage10@gmail.com>
2026-04-07 12:41:25 +0545

    Created: EditandCreateContact

--- my-next-app/src/app/contact/new/page.tsx
+++ my-next-app/src/app/contact/new/page.tsx
@@ -1,7 +1,20 @@
+import ContactForm from "@/app/_components/ContactForm";
+import { createContactAction } from "@/app/actions/contact";
 import React from "react";
 
 function NewContactPage() {
-  return <div>NewContactPage</div>;
+  return (
+    <div className=" flex justify-center items-center">
+      <div className="w-full max-w-xl bg-white shadow-lg rounded-2xl p-6">
+        <h1 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
+          Create New Contact
+        </h1>
+
+        {/* Form */}
+        <ContactForm action={createContactAction} />
+      </div>
+    </div>
+  );
 }
 
 export default NewContactPage;
